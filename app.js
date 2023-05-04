9c08867386443ae95225b7ed86acc67df96a4d52
BisalBosu <bishalbosu@gmail.com>
2023-05-04 13:03:38 +0530

    initial backend

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config()
+
+const express = require("express")
+const morgan = require("morgan")
+const path = require("path")
+const fs = require("fs")
+const compression = require("compression")
+
+
+
+const bodyParser = require("body-parser")
+//const sequelize = require("./utils/database")
+
+
+//imported models
+
+
+
+//imported routes
+
+
+
+const app = express();
+
+//making the public file availavail to client side.
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json({ extended: false }));
+
+//for morgan
+const accessLogStream = fs.createWriteStream(
+	path.join(__dirname, "access.log"),
+	{flags: 'a'}
+)
+	
+//passing the middlewares
+app.use(compression());
+app.use(morgan("combined", {stream: accessLogStream}))
+
+
+app.use((req, res, next)=>{
+    res.redirect("/html/signup.html")
+})
+
+
+
+//sync with sequelize enries
+// sequelize
+// 	.sync()
+// 	.then((result) => {
+//         //either run on PORT variable if not availavail 3006
+// 		app.listen(process.env.PORT || 3009)
+// 	})
+// 	.catch((err) => console.log("DbErroRRR: ", err))
+
+app.listen(process.env.PORT || 3009)
